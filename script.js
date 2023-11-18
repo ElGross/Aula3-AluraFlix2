f082d3a7fe35c6320cee5c80326eb0df4c359064
ElGross <151382540+ElGross@users.noreply.github.com>
2023-11-18 13:17:36 -0300

    Lista filmes
    Lista com imagens de filmes

---
+++ script.js
@@ -0,0 +1,14 @@
+var filme1 = "https://br.web.img2.acsta.net/pictures/22/12/29/17/28/4059223.jpg"; //Re-Animator: A Hora dos Mortos-Vivos
+var filme2 = "https://br.web.img3.acsta.net/pictures/15/04/29/19/23/173745.jpg"; //O Ataque dos Tomates Assassinos
+var filme3 = "https://images.justwatch.com/poster/244597010/s718/rubber-pneu.jpg"; //Rubber, O Pneu Assassino
+var filme4 = "https://images.justwatch.com/poster/98124267/s718/palhacos-assasinos.jpg"; //Palhaços Assassinos do Espaço Sideral
+var filme5 = "https://m.media-amazon.com/images/M/MV5BODcwZWFiNTEtNDgzMC00ZmE2LWExMzYtNzZhZDgzNDc5NDkyXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg"; //Sharknado
+var filme6 = "https://br.web.img3.acsta.net/pictures/210/150/21015012_20130624195436012.jpg"; //Planeta Terror
+
+
+document.write("<img src=" + filme1 + ">");
+document.write("<img src=" + filme2 + ">");
+document.write("<img src=" + filme3 + ">");
+document.write("<img src=" + filme4 + ">");
+document.write("<img src=" + filme5 + ">");
+document.write("<img src=" + filme6 + ">");
